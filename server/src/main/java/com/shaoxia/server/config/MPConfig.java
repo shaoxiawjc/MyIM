b8b668607dc659f8bc42625481c9cfba7da914a3
shaoxiawjc <wjc2800@163.com>
2024-04-17 14:37:11 +0800

    add MPConfig

--- server/src/main/java/com/shaoxia/server/config/MPConfig.java
+++ server/src/main/java/com/shaoxia/server/config/MPConfig.java
@@ -1,10 +1,26 @@
 package com.shaoxia.server.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author wjc28
  * @version 1.0
  * @description: mybatis配置类
  * @date 2024-04-13 0:04
  */
+@Configuration
 public class MPConfig {
+	/**
+	 * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+	 */
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		return interceptor;
+	}
 }
